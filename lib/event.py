cce3e539245f6f01abfb6ed2de64e7e5bd81285d
Chris White <cmwhite@L7EIS-ISA014.corp.costco.com>
2014-12-10 21:51:34 -0800

    b

--- lib/event.py
+++ lib/event.py
@@ -235,17 +235,17 @@ class Event(object):
         if self._outDir not in self._baseFilePath:
             self._outDir = os.path.dirname(os.path.abspath(self._baseFilePath))
 
-        try:
-            os.makedirs(os.path.join(self._outDir, 'bin'))
-            if self._outDirGroup:
-                os.chown(self._outDir, -1, grp.getgrnam(self._outDirGroup).gr_gid)
-                for root, dirs, files in os.walk(self._outDir):  
-                    for momo in dirs:  
-                        os.chown(os.path.join(root, momo), -1, grp.getgrnam(self._outDirGroup).gr_gid)
-                    for momo in files:
-                        os.chown(os.path.join(root, momo), -1, grp.getgrnam(self._outDirGroup).gr_gid)
-        except(OSError):
-            pass
+#        try:
+        os.makedirs(os.path.join(self._outDir, 'bin'))
+        if self._outDirGroup:
+            os.chown(self._outDir, -1, grp.getgrnam(self._outDirGroup).gr_gid)
+            for root, dirs, files in os.walk(self._outDir):  
+                for momo in dirs:  
+                    os.chown(os.path.join(root, momo), -1, grp.getgrnam(self._outDirGroup).gr_gid)
+                for momo in files:
+                    os.chown(os.path.join(root, momo), -1, grp.getgrnam(self._outDirGroup).gr_gid)
+ #       except(OSError):
+ #           pass
 
                  
     def detectInputCases(self, input, yes=False, trailingChar='\\b'):
