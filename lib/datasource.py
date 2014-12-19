24e426528aa594a05061bf7b855d130ee3cc94ca
Chris White <cmwhite@L7EIS-ISA014.corp.costco.com>
2014-12-18 22:11:35 -0800

    Adding DHCP Data source

--- lib/datasource.py
+++ lib/datasource.py
@@ -223,3 +223,211 @@ class DailyLogSource(object):
         else:
             return orf
 
+
+class ISOLogSource(object):
+    
+    def __init__(self, event):
+        self.event = event
+    
+    def pullDaily(self, egrepInclude, egrepExclude, 
+                  startDate, endDate, server, logpath, outputExtension, 
+                  compressionDelay, compressionExtension, formalName, toFile=True, toStdOut=False, 
+                  collect=False, formatter=None, header=None, customCmd=None, append=False, retResults=False):
+        
+        print("Pulling logs from %s to %s\n" % (startDate.date().isoformat(), endDate.date().isoformat()))
+        
+        if customCmd:
+            cmd = customCmd
+        elif not egrepExclude:
+            cmd = 'egrep "%s"' % (egrepInclude)
+        else:
+            cmd = 'egrep "%s" | egrep -v "%s"' % (egrepInclude, egrepExclude)
+        
+        print('command: ... | %s\n' % cmd)
+        
+        today = datetime.datetime.today()
+        oneDay = datetime.timedelta(days=1)
+        
+        fileName = os.path.basename(logpath)
+        dirName = os.path.dirname(logpath)
+        
+        server = initSSH(server)
+        
+        tick = time()
+        
+        output = ""
+        
+        orf = '%s.%s' % (self.event._baseFilePath, outputExtension)
+        if append:
+            outRawFile = open(orf, 'a')
+        else:
+            outRawFile = open(orf, 'w')
+        if formatter:
+            off = '%s.%s' % (self.event._baseFilePath, outputExtension + 'f')
+            if append:
+                outFileFormatted = open(off, 'a')
+            else:
+                outFileFormatted = open(off, 'w')
+        
+        if header and toStdOut and not collect:
+            print(header)
+        
+        while startDate <= endDate:
+            currentDate = startDate.strftime("%Y%m%d") 
+            
+            if today.date() != datetime.datetime.today().date():
+                print('WARNING: it appears the log pull was still in progress beyond midnight.')
+                print('There is a strong likelihood that log data will be missed.')
+            
+            
+            if (today.date() - startDate.date()).days > int(compressionDelay):
+                if compressionExtension == 'bz2' or compressionExtension == 'gz':
+                    if compressionExtension == 'bz2':
+                        catCmd = 'bzcat'
+                    elif compressionExtension == 'gz':
+                        catCmd = 'zcat'
+                
+                    if not toStdOut or collect:
+                        print("Checking %s.%s.log.%s..." % (fileName, currentDate, compressionExtension))
+                    fullcmd = '%s %s.%s.log.%s | %s' % (catCmd, logpath, currentDate, compressionExtension, cmd)
+                    stdin, stdout, stderr = server.exec_command(fullcmd)
+                    #print stderr.readlines()
+                else:
+                    '''To add additional compression functionality to the daily pull please elif this block and add your logic here.'''
+                    logging.warning('The daily logs you are attempting to pull are in a compression format, %s, that is currently not implemented' % (compressionExtension))
+            else:
+                if not toStdOut or collect:
+                    print("Checking %s.%s.log..." % (fileName, currentDate))
+                fullcmd = 'cat %s.%s.log | %s' % (logpath, currentDate, cmd)
+                stdin, stdout, stderr = server.exec_command(fullcmd)
+                #print stderr.readlines()
+                
+            startDate += oneDay
+                    
+            if collect:
+                output += stdout.read()
+                #output += 'tada\n'
+            else:
+                out = stdout.read()
+                #out = 'tada'
+                if formatter:
+                    outf = formatter(out)
+                    outFileFormatted.write(outf)
+                    outFileFormatted.flush()
+                
+                if toFile:
+                    outRawFile.write(out)
+                    outRawFile.flush()
+                                    
+                if toStdOut:
+                    if formatter:
+                        print(outf)
+                    else:
+                        print(out)
+                    
+        if collect:
+            if formatter:
+                outf = formatter(output)
+                outFileFormatted.write(outf)
+                outFileFormatted.flush()
+                
+            if toFile:
+                outRawFile.write(output)
+                outRawFile.flush()
+                
+            if toStdOut:
+                print('\n\nRetrieval time: %s\n' % (str(time() - tick)))
+                print('')
+                if header:
+                    print(header)
+                if formatter:
+                    print(outf)
+                else:
+                    print(output)
+        
+        if formatter:
+            outFileFormatted.close()
+        outRawFile.close()
+                
+                
+        if not toStdOut:
+            print('\n\nRetrieval time: %s\n' % (str(time() - tick)))
+            
+        
+        if formatter:
+            print('\nFormatted %s results saved to: %s' % (formalName, off))
+            print('Raw %s results saved to: %s' % (formalName, orf))
+            
+        elif toFile:
+            print('\nRaw %s results saved to: %s' % (formalName, orf))
+        print('')
+        
+        if retResults:
+            return output
+        else:
+            return orf
+            
+        
+    def pullGenericDaily(self, egrepInclude, egrepExclude, 
+                  startDate, endDate, server, logpath, outputExtension, 
+                  compressionDelay, compressionExtension, toFile=True, toStdOut=False, 
+                  customCmd=None, append=False, retResults=False):
+        
+        if customCmd:
+            cmd = customCmd
+        elif not egrepExclude:            
+            cmd = 'egrep "%s"' % (egrepInclude)
+        else:
+            cmd = 'egrep "%s" | egrep -v "%s"' % (egrepInclude, egrepExclude)
+        
+        today = datetime.datetime.today()
+        oneDay = datetime.timedelta(days=1)
+        
+        fileName = os.path.basename(logpath)
+        
+        server = initSSH(server)
+        
+        tick = time()
+        
+        output = ""
+
+        while startDate <= endDate:
+            daysBack = (today.date() - startDate.date()).days - 1
+            
+            if today.date() != datetime.datetime.today().date():
+                print('WARNING: it appears the log pull was still in progress beyond midnight.')
+                print('There is a strong likelihood that log data will be missed.')
+            
+            if daysBack == -1:
+                #if not toStdOut or collect:
+                print("Checking %s..." % (fileName))
+                fullcmd = 'cat %s | %s' % (logpath, cmd)
+                stdin, stdout, stderr = server.exec_command(fullcmd)
+                #print stderr.readlines()
+            elif daysBack > compressionDelay:
+                if compressionExtension == 'bz2':
+                    #if not toStdOut or collect:
+                    print("Checking %s.%s.%s..." % (fileName, str(daysBack), compressionExtension))
+                    fullcmd = 'bzcat %s.%s.%s | %s' % (logpath, str(daysBack), compressionExtension, cmd)
+                    stdin, stdout, stderr = server.exec_command(fullcmd)
+                    #print stderr.readlines()
+                else:
+                    '''To add additional compression functionality to the daily pull please elif this block and add your logic here.'''
+                    logging.warning('The daily logs you are attempting to pull are in a compression format, %s, that is currently not implemented' % (compressionExtension))
+            else:
+                #if not toStdOut or collect:
+                print("Checking %s.%s..." % (fileName, str(daysBack)))
+                fullcmd = 'cat %s.%s | %s' % (logpath, str(daysBack), cmd)
+                stdin, stdout, stderr = server.exec_command(fullcmd)
+                #print stderr.readlines()
+                
+            startDate += oneDay
+                    
+            
+            output += stdout.read()
+
+                
+        if retResults:
+            return output
+        else:
+            return orf
---
+++ plugins/sources/dhcp.py
@@ -0,0 +1,129 @@
+'''
+Copyright (c) 2014 Chris White
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+import datetime, re
+from lib.datasource import ISOLogSource 
+from lib.util import getIPAddress, getMACAddress, uniq, getTimeBisect
+
+
+def input(event):
+    inputHeader = '%s Query Options' % FORMAL_NAME
+    event.setOutPath()
+    event.setDateRange()
+
+
+def adhocInput(event):
+    inputHeader = '%s Query Options' % FORMAL_NAME
+    event.setOutPath()
+    event.setDateRange()
+    event.setAttribute('_include', prompt='Include', header=inputHeader)
+    event.setAttribute('_include', event.detectInputCases(event._include), force=True)
+    
+
+def execute(event):
+    
+    if not event.adHoc:
+        if hasattr(event, 'ip_address'):
+            event._include = event.detectInputCases(event.ip_address, yes=True, trailingChar='\\b')
+        else:
+            event._include = event.detectInputCases(ip_address, yes=True, trailingChar='\\b')
+
+    def dhcpFormatter(input):
+        remove = ['to ', 'for ', 'on ', 'from ']
+        formatted = ['%-20s %-8s %s' % ('Date/Time', 'Type', 'Message')]
+        formatted.append('-' * 80)
+        for line in uniq(input.splitlines()):
+            sline = line.split(']:')
+            time = datetime.datetime.strptime(' '.join(sline[0].split()[:3]), '%b %d %H:%M:%S')
+            msg =  sline[1].split()
+            for r in remove:
+                msg = msg.replace(r, '')
+            msg = msg.split('via')[0].split()
+            formatted.append('%s  %-8s %s' % (time.strftime('%Y-%m-%d %H:%M:%S'), msg[0].split('DHCP')[1], ' '.join(msg[1:])))
+            
+        formatted.append('')
+            
+        return '\n'.join(uniq(formatted))
+       
+    def getHostName(input):
+        hostname = re.search(r"\([a-zA-Z0-9_]+\) via", input)
+        if hostname:
+            return hostname.group().split()[0].strip('()').lower()
+        else:
+            return None
+         
+    event.setAttribute('_customDHCPCmd', value='egrep "DHCPREQUEST|DHCPACK|DHCPNACK|DHCPRELEASE|DHCPOFFER" | egrep "%s"' % event._include)
+    ils = ISOLogSource(event)
+    
+    if event.adHoc:
+        results = ils.pullDaily(egrepInclude=None, 
+                                egrepExclude=None, 
+                                startDate=event._startDate, 
+                                endDate=event._endDate, 
+                                server=confVars.server, 
+                                logpath=confVars.logpath, 
+                                outputExtension=confVars.outputExtension, 
+                                compressionDelay=confVars.compressionDelay, 
+                                compressionExtension=confVars.compressionExtension, 
+                                formalName=FORMAL_NAME,
+                                toFile=True, 
+                                toStdOut=True, 
+                                collect=True, 
+                                formatter=dhcpFormatter,
+                                customCmd=event._customDHCPCmd,
+                                retResults=True)
+    else:
+        results = ils.pullDaily(egrepInclude=None, 
+                                egrepExclude=None, 
+                                startDate=event._startDate, 
+                                endDate=event._endDate, 
+                                server=confVars.server, 
+                                logpath=confVars.logpath, 
+                                outputExtension=confVars.outputExtension, 
+                                compressionDelay=confVars.compressionDelay, 
+                                compressionExtension=confVars.compressionExtension, 
+                                formalName=FORMAL_NAME,
+                                toFile=True, 
+                                toStdOut=False, 
+                                collect=True, 
+                                formatter=dhcpFormatter,
+                                customCmd=event._customDHCPCmd,
+                                retResults=True)
+
+    if not event.adHoc:
+        
+        event._splunk.push(sourcetype=confVars.splunkSourcetype, filename='%s.%s' % (event._baseFilePath, confVars.outputExtension))
+        
+        before, after = getTimeBisect(event._DT, results)
+        
+        for line in reversed(before):
+            hostname = getHostName(line)
+            if hostname:
+                event.setAttribute('hostname', hostname)
+                break
+        
+        for line in reversed(before):
+            if 'DHCPACK' in line:
+                if getIPAddress(line) == event.ip_address:
+                    event.setAttribute('mac_address', getMACAddress(line))
+                    return
+                
+        for line in after:
+            if 'DHCPACK' in line:
+                if getIPAddress(line) == event.ip_address:
+                    event.setAttribute('mac_address', getMACAddress(line))
+                    return
+                
+        
