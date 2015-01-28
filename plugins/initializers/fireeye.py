1c573747993d865ac338af840e12218ca13a1107
Chris White <cmwhite@L7EIS-ISA014.corp.costco.com>
2015-01-27 21:45:29 -0800

    b

--- plugins/initializers/fireeye.py
+++ plugins/initializers/fireeye.py
@@ -28,7 +28,7 @@ def execute(event):
         
     query = '''search index=fireeye alert.id="%s" | table alert.occurred alert.src.ip alert.src.mac alert.dst.ip alert.dst.mac alert.name "alert.explanation.malware-detected.malware.name"''' % (event.fireID)        
     
-    print('Checking Splunk...'),
+    print('\nChecking Splunk...'),
     #try:
     print query
         
@@ -40,43 +40,44 @@ def execute(event):
     
     print('Done\n')
     
-    if results:
-        timestamp = results[0]['alert.occurred'].split('+')[0]
-        srcIP = results[0]['alert.src.ip']
-        srcMAC = results[0]['alert.src.mac']
-        dstIP = results[0]['alert.dst.ip']
-        dstMAC = results[0]['alert.dst.mac']
-        signature = '%s - %s' % (results[0]['alert.name'], results[0]['alert.explanation.malware-detected.malware.name'])
+    if not results:
+        log.error("Error: unable to pull FireEye ID event details from Splunk")
+        exit()
+    timestamp = results[0]['alert.occurred'].split('+')[0]
+    srcIP = results[0]['alert.src.ip']
+    srcMAC = results[0]['alert.src.mac']
+    dstIP = results[0]['alert.dst.ip']
+    dstMAC = results[0]['alert.dst.mac']
+    signature = '%s - %s' % (results[0]['alert.name'], results[0]['alert.explanation.malware-detected.malware.name'])
 
-        event.setOutPath(event.fireID)
-        
-        # Note the utc offset for the US will always be -x so by adding the offset you are adding a negative, i.e. subtracting
-        # This is very important for accurate time conversion.  You should always add the offset if the time is in UTC and
-        # subtract the offset if the time is local.  If the reverse makes more sense to you, event._absUTCOffsetTimeDelta
-        # is available
-        # Also note, setEventDateTime is called twice to initialize utcOffsetTimeDelta then adjust.
-        event.setEventDateTime(datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S'))
-        event.setEventDateTime(event._DT + event._utcOffsetTimeDelta)
-        
-        print('\nLocal Timestamp      Source IP        Destination IP   Signature')
-        print('-' * 80)
-        print('%-20s %-16s %-16s %s\n' % (event._DT.strftime('%Y-%m-%d %H:%M:%S'), srcIP, dstIP, signature))
-        
-        event.setAttribute('Event_Date/Time', event._DT.strftime('%Y-%m-%d %H:%M:%S'))
-        
-        ans = getUserInWithDef('Track source or destination (s/d)', 's')
-        if 's' in ans:
-            event.setAttribute('ip_address', srcIP)
-            event.setAttribute('mac_address', srcMAC)
-        elif 'd' in ans:
-            event.setAttribute('ip_address', dstIP)
-            event.setAttribute('mac_address', dstMAC)
-        else:
-            event.setAttribute('ip_address', prompt='IP Address', default=ans, description='Neither the source or destination was chosen, please confirm.')
-        
-        print('')
-        
-        event.setAttribute('description', prompt='Description', default=signature)
-        event.setDateRange()
-            
+    event.setOutPath(event.fireID)
     
+    # Note the utc offset for the US will always be -x so by adding the offset you are adding a negative, i.e. subtracting
+    # This is very important for accurate time conversion.  You should always add the offset if the time is in UTC and
+    # subtract the offset if the time is local.  If the reverse makes more sense to you, event._absUTCOffsetTimeDelta
+    # is available
+    # Also note, setEventDateTime is called twice to initialize utcOffsetTimeDelta then adjust.
+    event.setEventDateTime(datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S'))
+    event.setEventDateTime(event._DT + event._utcOffsetTimeDelta)
+    
+    print('\nLocal Timestamp      Source IP        Destination IP   Signature')
+    print('-' * 80)
+    print('%-20s %-16s %-16s %s\n' % (event._DT.strftime('%Y-%m-%d %H:%M:%S'), srcIP, dstIP, signature))
+    
+    event.setAttribute('Event_Date/Time', event._DT.strftime('%Y-%m-%d %H:%M:%S'))
+    
+    ans = getUserInWithDef('Track source or destination (s/d)', 's')
+    if 's' in ans:
+        event.setAttribute('ip_address', srcIP)
+        event.setAttribute('mac_address', srcMAC)
+    elif 'd' in ans:
+        event.setAttribute('ip_address', dstIP)
+        event.setAttribute('mac_address', dstMAC)
+    else:
+        event.setAttribute('ip_address', prompt='IP Address', default=ans, description='Neither the source or destination was chosen, please confirm.')
+    
+    print('')
+    
+    event.setAttribute('description', prompt='Description', default=signature)
+    event.setDateRange()
+        
